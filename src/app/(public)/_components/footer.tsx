dae48f790d325b11cc29af53b128e215e40dead6
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2025-11-04 16:41:50 -0300

    Adiciona componente Footer à página inicial e implementa estrutura do rodapé

---
+++ src/app/(public)/_components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className="py-6 text-center text-gray-500 text-sm md:text-base">
+            <p>Todos direitos reservdos © {new Date().getFullYear()} <span className="hover:text-black"><a href="https://www.linkedin.com/in/evertonbrandao/">@evertonbrandao</a></span></p>
+        </footer>
+    )
+}
