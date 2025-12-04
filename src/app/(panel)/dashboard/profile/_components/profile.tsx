729c33a8555a05e6326318c7e9943952bf665280
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2025-12-04 19:40:50 -0300

    Adiciona funcionalidade para configurar horários da clínica com seleção de fuso horário; implementa diálogo para seleção de horários disponíveis

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from 'react';
 import { useProfileForm } from './profile_form';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
@@ -7,10 +8,39 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { Label } from '@/components/ui/label';
 import Image from 'next/image';
 import imgTest from '../../../../../../public/foto1.png';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 const ProfileContent = () => {
 
+    const [selectedHours, setSelectedHours] = useState<string[]>([]);
+    const [dialogOpen, setDialogOpen] = useState(false);
+
     const form = useProfileForm();
+
+    function generateTimeSlot(): string[] {
+        const hours: string[] = [];
+
+        for (let i = 8; i <= 23; i++) {
+            for (let j = 0; j < 2; j++) {
+                const hour = i.toString().padStart(2, '0');
+                const minute = (j * 30).toString().padStart(2, '0');
+                hours.push(`${hour}:${minute}`);
+            }
+        }
+
+        return hours;
+    }
+
+    const hours = generateTimeSlot();
+
+    const handleHourClick = (hour: string) => {
+        setSelectedHours((prev) => prev.includes(hour) ? prev.filter(h => h !== hour) : [...prev, hour].sort());
+    };
+
+    const timeZones = Intl.supportedValuesOf('timeZone').filter(zone => zone.includes('America/Sao_Paulo') || zone.includes('America/Fortaleza') || zone.includes('America/Recife') || zone.includes('America/Bahia') || zone.includes('America/Belem') || zone.includes('America/Manaus') || zone.includes('America/Cuiaba') || zone.includes('America/Boa_Vista') || zone.includes('America/Rio_Branco'));
 
     return (
         <div className='mx-auto'>
@@ -91,6 +121,75 @@ const ProfileContent = () => {
                                         </FormItem>
                                     )}
                                 />
+                                <div className='space-y-2'>
+                                    <Label className='font-semibold'>
+                                        Configurar hórarios da clínica
+                                    </Label>
+
+                                    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+                                        <DialogTrigger asChild>
+                                            <Button variant={"outline"} className='w-full justify-between'>
+                                                Clique aqui para selecionar hórarios
+                                                <ArrowRight className='w-5 h-5' />
+                                            </Button>
+                                        </DialogTrigger>
+                                        <DialogContent>
+                                            <DialogHeader>
+                                                <DialogTitle>
+                                                    Configurar hórarios da clínica
+                                                </DialogTitle>
+                                                <DialogDescription>
+                                                    Aqui você pode configurar os hórarios de funcionamento da clínica.
+                                                </DialogDescription>
+                                            </DialogHeader>
+                                            <section className='py-4'>
+                                                <p className='text-sm text-muted-foreground mb-2'>
+                                                    Clique nos horários abaixo para marcar ou desmarcar
+                                                </p>
+
+                                                <div className='grid grid-cols-5 gap-2'>
+                                                    {hours.map((hour) => (
+                                                        <Button key={hour} variant={"outline"} className={cn('h-10', selectedHours.includes(hour) && 'border-2 border-emerald-500 text-primary')}
+                                                            onClick={() => handleHourClick(hour)}
+                                                        >
+                                                            {hour}
+                                                        </Button>
+                                                    ))}
+                                                </div>
+                                            </section>
+                                            <Button onClick={() => setDialogOpen(false)} className='w-full'>
+                                                Fechar
+                                            </Button>
+                                        </DialogContent>
+                                    </Dialog>
+                                </div>
+
+                                <FormField
+                                    control={form.control}
+                                    name={"timeZone"}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel className='font-semibold'>Selecione o fuso horário</FormLabel>
+                                            <FormControl>
+                                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                                    <SelectTrigger>
+                                                        <SelectValue placeholder={"Selecione seu fuso horário"} />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        {timeZones.map((zone) => (
+                                                            <SelectItem key={zone} value={zone}>{zone}</SelectItem>
+                                                        ))}
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+
+                                <Button type={'submit'} className={"w-full bg-emerald-500 hover:bg-emerald-700 cursor-pointer"}>
+                                    Salvar alterações
+                                </Button>
                             </div>
                         </CardContent>
                     </Card>
--- src/app/(panel)/dashboard/profile/page.tsx
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -1,13 +1,6 @@
 import ProfileContent from "./_components/profile";
-import { getUserData } from "./_data-access/get-info-user";
-import getSession from "@/lib/getSession";
 
-const Profile = async () => {
-
-  const session = await getSession();
-
-  const user = await getUserData({ userId: session?.user?.id! });
-  
+const Profile = async () => {  
 
   return (
     <ProfileContent />
